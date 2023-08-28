927fcaa8fe3e0bc8fc377f2b780db512db4115ae
Leo9428 <127988282+Leo9428@users.noreply.github.com>
2023-08-28 14:47:28 -0300

    feat: crear archivos de validaciones

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,30 @@
 // routes.mjs
 
-import express from "express";
-import { body, param } from "express-validator";
-import ctrlUsuarios from "../controller/user.controller.js"; // Asegúrate de ajustar las rutas
-import ctrlProyectos from "../controller/proyecto.controller.js"; // Asegúrate de ajustar las rutas
-import ctrlTareas from "../controller/tareas.controller.js"; // Asegúrate de ajustar las rutas
+import { Router } from "express";
+import ctrlUsuarios from "../controller/user.controller.js";
+import ctrlProyectos from "../controller/proyecto.controller.js";
+import ctrlTareas from "../controller/tareas.controller.js";
+import { validateSchema } from "../middleware/validationSchema.js";
+import { validar } from "../models/schema.js";
+import { validarProyecto } from "../models/shema.proyecto.js";
+import { validarTarea } from "../models/schema.tareas.js";
+const router = Router();
 
-const router = express.Router();
 // ... Rutas para Usuario ...
+router.post(
+  "/crearusuario",
+  validar,
+  validateSchema,
+  ctrlUsuarios.crearUsuario
+);
 
-// ... Rutas para Proyecto ...
+router.post(
+  "/crearproyecto",
+  validarProyecto,
+  validateSchema,
+  ctrlProyectos.crearProyecto
+);
 
-// ... Rutas para Tareas ...
-
-export default router; // Exportación del enrutador
+router.post("/creartarea", validarTarea, validateSchema, ctrlTareas.crearTarea);
+// Exportación del enrutador
+export default router;
