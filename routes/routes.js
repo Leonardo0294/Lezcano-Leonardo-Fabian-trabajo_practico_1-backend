bed66a35714789b27aa3b70df4dcc5c3e7f7dead
Leo9428 <127988282+Leo9428@users.noreply.github.com>
2023-08-28 15:08:18 -0300

    feat: agregar archivo de la base de datos

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,3 @@
-// routes.mjs
-
 import { Router } from "express";
 import ctrlUsuarios from "../controller/user.controller.js";
 import ctrlProyectos from "../controller/proyecto.controller.js";
@@ -8,23 +6,35 @@ import { validateSchema } from "../middleware/validationSchema.js";
 import { validar } from "../models/schema.js";
 import { validarProyecto } from "../models/shema.proyecto.js";
 import { validarTarea } from "../models/schema.tareas.js";
+
+// Crear una instancia de Router de Express
 const router = Router();
 
-// ... Rutas para Usuario ...
+// Definición de rutas y configuración de controladores y validaciones
+
+// Ruta para crear un usuario
 router.post(
   "/crearusuario",
-  validar,
-  validateSchema,
-  ctrlUsuarios.crearUsuario
+  validar, // Middleware para validar la solicitud
+  validateSchema, // Middleware para validar el esquema de datos
+  ctrlUsuarios.crearUsuario // Controlador para crear un usuario
 );
 
+// Ruta para crear un proyecto
 router.post(
   "/crearproyecto",
-  validarProyecto,
-  validateSchema,
-  ctrlProyectos.crearProyecto
+  validarProyecto, // Middleware para validar el esquema de proyecto
+  validateSchema, // Middleware para validar el esquema de datos
+  ctrlProyectos.crearProyecto // Controlador para crear un proyecto
 );
 
-router.post("/creartarea", validarTarea, validateSchema, ctrlTareas.crearTarea);
-// Exportación del enrutador
+// Ruta para crear una tarea
+router.post(
+  "/creartarea",
+  validarTarea, // Middleware para validar el esquema de tarea
+  validateSchema, // Middleware para validar el esquema de datos
+  ctrlTareas.crearTarea // Controlador para crear una tarea
+);
+
+// Exportación del enrutador configurado
 export default router;
